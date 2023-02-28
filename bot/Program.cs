c3f08dc72fa77104f89d65fbc0ecebdb58119cde
Vincent <vincent.burkhardt@studmail.htw-aalen.de>
2023-02-28 21:13:49 +0100

    Initial commit

---
+++ bot/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Reflection;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Discord;
+
+/// <summary>
+/// tinkering with discord.NET Interactions.
+/// just a playground 
+/// </summary>
+
+public class program
+{
+    
+    private static DiscordSocketClient? discordClient;
+
+    // Calls async method because fuck Main for not being async
+    public static Task Main(string[] args) => new program().MainAsync();
+
+    async Task MainAsync()
+    {
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((_, services) => services
+                .AddSingleton(_ => new DiscordSocketClient(new DiscordSocketConfig
+                {
+                    GatewayIntents = Discord.GatewayIntents.AllUnprivileged,
+                    AlwaysDownloadUsers = true,
+                }))
+                .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                .AddSingleton<InteractionHandler>())
+                .UseConsoleLifetime()
+            .Build();
+        
+        await host.StartAsync();
+        await RunAsync(host);
+    }
+
+    private async Task RunAsync(IHost host)
+    {
+        using IServiceScope serviceScope = host.Services.CreateScope();
+        IServiceProvider provider = serviceScope.ServiceProvider;
+        
+        discordClient = provider.GetRequiredService<DiscordSocketClient>();
+        var interactionService = provider.GetRequiredService<InteractionService>();
+        
+        await provider.GetRequiredService<InteractionHandler>().InitializeAsync();
+
+        discordClient.Ready += async () =>
+        {
+            await interactionService.RegisterCommandsGloballyAsync();
+        };
+
+        // Get the token from the environment variable (thanks for scanning for credentials GitHub <3)
+        var TOKEN = Environment.GetEnvironmentVariable("discordToken");
+        await discordClient.LoginAsync(Discord.TokenType.Bot, TOKEN);
+        
+        await discordClient.StartAsync();
+        await Task.Delay(-1);
+    }
+    
+    public class InteractionHandler
+    {
+        DiscordSocketClient client;
+        InteractionService interactionService;
+        IServiceProvider serviceProvider;
+
+        // Using constructor injection
+        public InteractionHandler(DiscordSocketClient client, InteractionService interactionService, IServiceProvider serviceProvider)
+        {
+            this.client = client;
+            this.interactionService = interactionService;
+            this.serviceProvider = serviceProvider;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), serviceProvider);
+            client.InteractionCreated += HandleSocketInteraction;
+            
+            interactionService.SlashCommandExecuted += (info, context, result) => 
+                { Console.WriteLine($"Slash command {info.Name} executed"); return Task.CompletedTask; };
+        }
+
+        /// <summary>
+        /// This function handles an interaction from a socket (such as a slash command)
+        /// </summary>
+        /// <param name="interaction"></param>
+        private async Task HandleSocketInteraction(SocketInteraction interaction) {
+            // Try to execute the interaction as a command
+            try {
+                // Create a context object for the interaction
+                var context = new SocketInteractionContext(client, interaction);
+                // Execute the command using the interaction service and the service provider
+                await interactionService.ExecuteCommandAsync(context, serviceProvider);
+            }
+            // Catch any exceptions that may occur
+            catch (Exception error) {
+                // If the interaction was an application command (slash command)
+                if (interaction.Type == InteractionType.ApplicationCommand) {
+                    // Delete the original response message from the bot
+                    await interaction.GetOriginalResponseAsync().ContinueWith(async (message) => await message.Result.DeleteAsync());
+                }
+            }
+        }
+    }
+}
---
+++ bot/SlashModules.cs
@@ -0,0 +1,69 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace bot;
+
+public class SlashModules{
+    
+    public class HelpCommand : InteractionModuleBase<SocketInteractionContext>{
+        // TODO
+        /// <summary>
+        /// Instead of listing and describing every fucking single command by hand,
+        /// I should try getting all commands from the Interaction Module ¯\_(ツ)_/¯
+        /// </summary>
+        [SlashCommand("help", "")]
+        public async Task Help(){
+            await RespondAsync("todo");
+        }
+    }
+
+    public class RandomImageModule : InteractionModuleBase<SocketInteractionContext>{
+        
+        [SlashCommand("image", "ein random image aus einem channel")]
+        public async Task RandomImageK([Discord.Commands.Summary("The name of the channel")] string channelName){
+            // Get the channel by name
+            var channel = Context.Guild.Channels.FirstOrDefault(x => x.Name == channelName) as SocketTextChannel;
+
+            if (channel == null){
+                await ReplyAsync("Falscher Channel Name.");
+                return;
+            }
+
+            // Get all the messages from the channel
+            var messages = await channel.GetMessagesAsync().FlattenAsync();
+
+            // Filter  messages that are actually images (attachments)
+            var images = messages.Where(x => x.Attachments.Count > 0).ToList();
+
+            if (images.Count == 0){
+                await ReplyAsync("Keine Bilder im Channel.");
+                return;
+            }
+
+            // just learnt this today xD
+            var random = new Random();
+            var index = random.Next(images.Count);
+            
+            // Send the image as a file 
+            var image = images[index];
+            var url = image.Attachments.First().Url;
+            var stream = await DownloadImageAsync(url);
+            await Context.Channel.SendFileAsync(stream, "random_image.jpg");
+        }
+
+        /// <summary>
+        /// The downloaded images from the helper function are stored in memory as streams,
+        /// not as files on disk. Therefore, they do not need to be deleted manually after the
+        /// image was successfully sent.
+        /// TODO: dispose the stream after using it to free up some memory resources.
+        /// </summary>
+        /// <param name="url">Discord's own URL for storing files</param>
+        /// <returns></returns>
+        private async Task<Stream> DownloadImageAsync(string url){
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            return await response.Content.ReadAsStreamAsync();
+        }
+    }
+}
